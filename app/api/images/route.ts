908485735361c4246c0062df95c3c6fc8599f59d
Raymart Maqueda <kuyamarqk@gmail.com>
2025-07-05 15:23:25 +0800

    Updating the images folder

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,4 +1,3 @@
-
 import fs from 'fs';
 import path from 'path';
 import { NextResponse } from 'next/server';
@@ -7,18 +6,18 @@ export async function GET() {
   const albums = ['bond', 'travel', 'latest'];
   const result: Record<string, { preview: string; count: number }> = {};
 
-  albums.forEach((album) => {
+  for (const album of albums) {
     const dirPath = path.join(process.cwd(), 'public', 'images', album);
     const files = fs.readdirSync(dirPath).filter((file) =>
       /\.(jpe?g|png|gif|webp|jfif)$/i.test(file)
     );
     if (files.length > 0) {
       result[album] = {
-        preview: `/images/${album}/${files[0]}`, // use first image as preview
+        preview: `/images/${album}/${files [0]}`, // use first image as preview
         count: files.length,
       };
     }
-  });
+  }
 
   return NextResponse.json(result);
 }
