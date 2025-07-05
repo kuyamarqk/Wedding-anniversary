6e7610b1ab4d6ea0165ac8f28343b54f1e7c2465
Raymart Maqueda <kuyamarqk@gmail.com>
2025-07-05 21:42:05 +0800

    putting the background image inside the images folder for organized code

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">npm
+    <html lang="en">
         <body className="min-h-screen scroll-smooth font-[Playfair Display] text-gray-900 relative">
           <NavBar />
         <div className="fixed inset-0 z-0 bg-[url('/images/background.jpeg')] bg-cover bg-center bg-no-repeat brightness-75" />
